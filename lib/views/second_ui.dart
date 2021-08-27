91b549293fc490cf7d2e3125f3bf3a8a1f2ed9ff
s6252410001 <s6252410001@sau.ac.th>
2021-08-27 14:36:59 +0700

    Code V.2 : Do yyyyyy

--- lib/views/second_ui.dart
+++ lib/views/second_ui.dart
@@ -11,7 +11,109 @@ class _SecondUIState extends State<SecondUI> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.yellow,
+      /* backgroundColor: Colors.yellow, */
+      appBar: AppBar(
+        backgroundColor: Colors.red,
+        title: Text(
+          'Second UI'
+        ),
+        centerTitle: true,
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.search,
+              ),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.exit_to_app,
+              color: Colors.grey[900],
+            )
+            ),
+        ],
+      ),
+      drawer: Drawer(
+        child: ListView (
+          padding: EdgeInsets.zero,
+          children: [
+            UserAccountsDrawerHeader(
+              currentAccountPicture: CircleAvatar(
+                backgroundImage: AssetImage(
+                  'assets/images/myprofile.jpg',
+                ),
+              ),
+              accountName: Text(
+                'Apisit Kulkham'
+              ),
+              accountEmail: Text(
+                'ID: 6252410001'
+              ),
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage(
+                    'assets/images/bgdrawer.png'
+                  ),
+                  fit: BoxFit.fill
+                )
+              ),
+              otherAccountsPictures: [
+                Image.asset(
+                  'assets/images/saulogo.png'
+                ),
+              ],
+            ),
+            ListTile(
+              onTap: () {},
+              title: Text(
+                'หน้าแรก',
+              ),
+            ),
+            ListTile(
+              onTap: () {},
+              title: Text(
+                'ตารางสอน'
+              ),
+              leading: Icon(
+                Icons.table_chart,
+                color: Colors.blue,
+              ),
+            ),
+            ListTile(
+              onTap: () {},
+              title: Text(
+                'ตารางสอบ'
+              ),
+              leading: Icon(
+                Icons.access_alarm
+              ),
+              trailing: Text(
+                '9999',
+                style: TextStyle(
+                  color: Colors.red,
+                  fontWeight: FontWeight.bold
+                ),
+              ),
+            ),
+            Divider(
+              color: Colors.red,
+            ),
+            ListTile(
+              onTap: () {},
+              title: Text(
+                'ออกจากโปรแกรม',
+              ),
+              trailing: Icon(
+                Icons.exit_to_app,
+                color: Colors.blue,
+              ),
+            ),
+          ],
+        ),
+      ),
+    
+    
     );
   }
 }
---
+++ lib/views/tird_ui.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+
+class TirdUI extends StatefulWidget {
+  const TirdUI({ Key? key }) : super(key: key);
+
+  @override
+  _TirdUIState createState() => _TirdUIState();
+}
+
+class _TirdUIState extends State<TirdUI> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
